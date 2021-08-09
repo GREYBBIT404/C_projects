a8b083fa14609ac99706254872ef2c0d315830df
Dylanl <greybbit@protonamil.com>
2021-08-09 21:23:30 +0100

    New commits

--- 3N+1/3nPlus1Calc.c
+++ 3N+1/3nPlus1Calc.c
@@ -5,13 +5,19 @@ main()
 {
 	int fNum, step;
 
-	printf("Enter int: ");
+	printf("Enter a number for range 1-100000000: ");
 	scanf("%d", &fNum);
+	while(fNum<1 || fNum>100000000){
+		printf("[**[Number outside given renage!]**]\n");
+		printf("Enter a number for range 1-100000000: ");
+		scanf("%d", &fNum);
+	}
+
 	step = 0;
 
 	while(fNum!=1){
 
-		printf("%d\n", fNum);
+		printf("Step %d:\t%d\n", step, fNum);
 
 		if(fNum%2==0){
 			fNum = fNum/2;
@@ -22,9 +28,9 @@ main()
 			step++;
 		}
 		else{
-			printf("TEST ERROR");
+			printf("ERROR");
 		}
 	}
-	printf("%d\n", fNum);
+	printf("Step %d:\t%d\n", step, fNum);
 	printf("Number of steps to loop: %d\n", step );
 }
